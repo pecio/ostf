db637b3164d9191b417239ae457c156da10a80cc
Raúl Pedroche <pedroche@me.com>
2022-04-04 21:21:16 +0200

    Create SSH security group in test

---
+++ test/common.tf
@@ -0,0 +1,17 @@
+resource "random_pet" "suffix" {
+  separator = "-"
+}
+
+resource "openstack_networking_secgroup_v2" "ssh" {
+  name        = "ssh-${random_pet.suffix.id}"
+  description = "Allow SSH access for testing"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "ssh" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  security_group_id = openstack_networking_secgroup_v2.ssh.id
+}
--- test/webserver.tf
+++ test/webserver.tf
@@ -8,27 +8,27 @@ terraform {
   }
 }
 
-resource "openstack_compute_instance_v2" "webserver" {
-  name            = "webserver"
+resource "openstack_compute_instance_v2" "wsimage" {
+  name            = "wsimage-${random_pet.suffix.id}"
   image_name      = "wsimage"
   flavor_id       = "d2"
-  key_pair        = "bichejo"
-  security_groups = ["default", "ssh"]
+  key_pair        = var.ssh_key_name
+  security_groups = [openstack_networking_secgroup_v2.ssh.name]
 
   network {
     name = "private"
   }
 }
 
-resource "openstack_compute_floatingip_v2" "floatingip1" {
+resource "openstack_compute_floatingip_v2" "wsimage" {
   pool = "public"
 }
 
-resource "openstack_compute_floatingip_associate_v2" "fip1" {
-  floating_ip = openstack_compute_floatingip_v2.floatingip1.address
-  instance_id = openstack_compute_instance_v2.webserver.id
+resource "openstack_compute_floatingip_associate_v2" "wsimage" {
+  floating_ip = openstack_compute_floatingip_v2.wsimage.address
+  instance_id = openstack_compute_instance_v2.wsimage.id
 }
 
-output "webserver_address" {
-  value = openstack_compute_floatingip_v2.floatingip1.address
+output "wsimage_address" {
+  value = openstack_compute_floatingip_v2.wsimage.address
 }
