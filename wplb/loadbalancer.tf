da1bc6c532cf7a4d42ead2df4f76055927910d92
Raúl Pedroche <pedroche@me.com>
2022-04-08 18:36:45 +0200

    Add Load Balancer configuration (non working)

---
+++ wplb/loadbalancer.tf
@@ -0,0 +1,32 @@
+resource "openstack_lb_loadbalancer_v2" "frontend" {
+  name          = "frontend-${random_pet.suffix.id}"
+  vip_subnet_id = openstack_networking_subnet_v2.frontend.id
+}
+
+resource "openstack_lb_listener_v2" "http" {
+  protocol        = "HTTP"
+  protocol_port   = 80
+  loadbalancer_id = openstack_lb_loadbalancer_v2.frontend.id
+}
+
+resource "openstack_lb_pool_v2" "webservers" {
+  protocol    = "HTTP"
+  lb_method   = "ROUND_ROBIN"
+  listener_id = openstack_lb_listener_v2.http.id
+}
+
+resource "openstack_lb_member_v2" "webserver" {
+  count         = var.webserver_instances
+  pool_id       = openstack_lb_pool_v2.webservers.id
+  address       = openstack_compute_instance_v2.webserver[count.index].network[0].fixed_ip_v4
+  protocol_port = 80
+}
+
+resource "openstack_networking_floatingip_v2" "frontend" {
+  pool = "public"
+}
+
+resource "openstack_networking_floatingip_associate_v2" "frontend" {
+  floating_ip = openstack_networking_floatingip_v2.frontend.address
+  port_id     = openstack_lb_loadbalancer_v2.frontend.vip_port_id
+}
