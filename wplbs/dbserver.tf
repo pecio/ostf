4c7f3b49c5ff38013746301adead062f9b13949d
Raúl Pedroche <pedroche@me.com>
2022-04-16 22:14:49 +0200

    wplb*: Make dbvol attachment no delete_on_termination
    Fixes #4

--- wplbs/dbserver.tf
+++ wplbs/dbserver.tf
@@ -32,7 +32,7 @@ resource "openstack_compute_instance_v2" "dbserver" {
     source_type           = "volume"
     destination_type      = "volume"
     boot_index            = 1
-    delete_on_termination = true
+    delete_on_termination = false
   }
 
   user_data = file("../scripts/provision-db-vol.sh")
