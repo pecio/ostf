0168f11552bf46eeaf0b9b3dbd0b74b379a8a156
Raúl Pedroche <pedroche@me.com>
2022-04-18 05:36:17 +0200

    Correctly use subshells in local.sh

--- devstack/local.sh
+++ devstack/local.sh
@@ -2,7 +2,7 @@
 # Postinstall
 set -o xtrace
 # Generate Amphora image
-{
+(
   WORKDIR=$(/usr/bin/mktemp -d)
   cd "${WORKDIR}"
   /usr/bin/sudo /opt/stack/octavia/diskimage-create/diskimage-create.sh
@@ -12,9 +12,9 @@ set -o xtrace
     --private --project service amphora-x64-haproxy
   cd
   /bin/rm -rf "${WORKDIR}"
-}
+)
 # Create Octavia flavorprofile and flavor
-{
+(
   . ~/devstack/openrc admin admin
   openstack loadbalancer flavorprofile create --provider amphora \
     --name amphora-single-profile \
@@ -23,19 +23,19 @@ set -o xtrace
     --flavorprofile amphora-single-profile \
     --description "A non-high availability load balancer for testing." \
     --enable
-}
+)
 # Import public keys from authorized_keys
-{
+(
   . ~/devstack/openrc demo demo
   while read type key description; do
     if [[ -n "${description}" ]]; then
       openstack keypair create "${description}" --public-key <(echo "${type} ${key} ${description}")
     fi
   done < ~/.ssh/authorized_keys
-}
+)
 # Create "ssh" security group
-{
+(
   . ~/devstack/openrc demo demo
   openstack security group create ssh
   openstack security group rule create ssh --dst-port 22 --protocol tcp
-}
+)
