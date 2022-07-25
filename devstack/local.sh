46daf5e7eb2f78ce9c6a46be205d91230469893c
Raúl Pedroche <pedroche@me.com>
2022-07-25 19:08:20 +0200

    Fix Packer Security Group creation

--- devstack/local.sh
+++ devstack/local.sh
@@ -31,10 +31,10 @@ set -o xtrace
       --enable
   fi
 )
-# Create "ssh" security group
+# Create Packer security group
 (
-  . /devstack/openrc demo demo
-  if [[ -z "$(openstack security group list -f value -c name | grep '^packer$')" ]]; then
+  . ~/devstack/openrc demo demo
+  if [[ -z "$(openstack security group list -f value -c Name | grep '^packer$')" ]]; then
     openstack security group create --description "Packer security group" packer
     openstack security group rule create --description "SSH access" \
       --dst-port 22 --protocol tcp --ingress packer
