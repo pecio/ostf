ebcf483b8885e84f69ef482161165c705a163d47
Raúl Pedroche <pedroche@me.com>
2022-07-26 15:10:36 +0200

    Use PKCS12 container for certificate

--- wplbs/ssl.tf
+++ wplbs/ssl.tf
@@ -1,43 +1,9 @@
 resource "openstack_keymanager_secret_v1" "certificate" {
-  name                 = "certificate-${random_pet.suffix.id}"
-  payload_content_type = "text/plain"
-  payload              = file("../certs/server.pem")
-  secret_type          = "certificate"
-}
-
-resource "openstack_keymanager_secret_v1" "private_key" {
-  name                 = "private_key-${random_pet.suffix.id}"
-  payload_content_type = "text/plain"
-  payload              = file("../certs/server.key")
-  secret_type          = "private"
-  algorithm            = "RSA"
-}
-
-resource "openstack_keymanager_secret_v1" "intermediate" {
-  name                 = "intermediate-${random_pet.suffix.id}"
-  payload_content_type = "text/plain"
-  payload              = file("../certs/ca-int.pem")
-  secret_type          = "certificate"
-}
-
-resource "openstack_keymanager_container_v1" "tls_container" {
-  name = "tls_container-${random_pet.suffix.id}"
-  type = "certificate"
-
-  secret_refs {
-    name       = "certificate"
-    secret_ref = openstack_keymanager_secret_v1.certificate.secret_ref
-  }
-
-  secret_refs {
-    name       = "private_key"
-    secret_ref = openstack_keymanager_secret_v1.private_key.secret_ref
-  }
-
-  secret_refs {
-    name       = "intermediates"
-    secret_ref = openstack_keymanager_secret_v1.intermediate.secret_ref
-  }
+  name                     = "certificate-${random_pet.suffix.id}"
+  payload_content_type     = "application/octet-stream"
+  payload                  = filebase64("../certs/server.pfx")
+  payload_content_encoding = "base64"
+  secret_type              = "certificate"
 }
 
 resource "openstack_lb_listener_v2" "ssl_listener" {
@@ -45,5 +11,5 @@ resource "openstack_lb_listener_v2" "ssl_listener" {
   protocol                  = "TERMINATED_HTTPS"
   protocol_port             = 443
   loadbalancer_id           = openstack_lb_loadbalancer_v2.frontend.id
-  default_tls_container_ref = openstack_keymanager_container_v1.tls_container.container_ref
+  default_tls_container_ref = openstack_keymanager_secret_v1.certificate.secret_ref
 }
