7bb3cbdd1320d2e47b8b8b8f631e0e592eae35c8
Raúl Pedroche <pedroche@me.com>
2022-07-30 15:29:07 +0200

    Generate and use only pkcs12 secret in Barbican

--- wplbs/ssl.tf
+++ wplbs/ssl.tf
@@ -6,52 +6,11 @@ resource "openstack_keymanager_secret_v1" "pfx" {
   secret_type              = "certificate"
 }
 
-resource "openstack_keymanager_secret_v1" "server" {
-  name                 = "server-${random_pet.suffix.id}"
-  payload_content_type = "text/plain"
-  payload              = file("../certs/server.pem")
-  secret_type          = "certificate"
-}
-
-resource "openstack_keymanager_secret_v1" "serverkey" {
-  name                 = "serverkey-${random_pet.suffix.id}"
-  payload_content_type = "text/plain"
-  payload              = file("../certs/server.key")
-  secret_type          = "private"
-}
-
-resource "openstack_keymanager_secret_v1" "rootca" {
-  name                 = "rootca-${random_pet.suffix.id}"
-  payload_content_type = "text/plain"
-  payload              = file("../certs/ca.pem")
-  secret_type          = "certificate"
-}
-
-resource "openstack_keymanager_container_v1" "server" {
-  name = "server-${random_pet.suffix.id}"
-  type = "certificate"
-
-  secret_refs {
-    name       = "certificate"
-    secret_ref = openstack_keymanager_secret_v1.server.secret_ref
-  }
-
-  secret_refs {
-    name       = "private_key"
-    secret_ref = openstack_keymanager_secret_v1.serverkey.secret_ref
-  }
-
-  secret_refs {
-    name       = "intermediates"
-    secret_ref = openstack_keymanager_secret_v1.rootca.secret_ref
-  }
-}
-
 resource "openstack_lb_listener_v2" "ssl_listener" {
   name                      = "https-${random_pet.suffix.id}"
   protocol                  = "TERMINATED_HTTPS"
   protocol_port             = 443
   loadbalancer_id           = openstack_lb_loadbalancer_v2.frontend.id
-  default_tls_container_ref = openstack_keymanager_container_v1.server.container_ref
+  default_tls_container_ref = openstack_keymanager_secret_v1.pfx.id
   default_pool_id           = openstack_lb_pool_v2.webservers.id
 }
