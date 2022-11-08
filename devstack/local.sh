183636bde0b6cbfdf462de50bfc261ec69b5bc99
Raúl Pedroche <pedroche@me.com>
2022-11-08 22:59:33 +0100

    Use block for SG creation in local.sh

--- devstack/local.sh
+++ devstack/local.sh
@@ -32,12 +32,14 @@ set -o xtrace
   fi
 )
 # Create packer security group
-. ~/devstack/openrc demo demo
-if [[ -z "$(openstack security group list -f value -c Name | grep '^packer$')" ]]; then
-  openstack security group create --description "Packer security group" packer
-  openstack security group rule create --description "SSH access" \
-    --dst-port 22 --protocol tcp --ingress packer
-fi
+(
+  . ~/devstack/openrc demo demo
+  if [[ -z "$(openstack security group list -f value -c Name | grep '^packer$')" ]]; then
+    openstack security group create --description "Packer security group" packer
+    openstack security group rule create --description "SSH access" \
+      --dst-port 22 --protocol tcp --ingress packer
+  fi
+)
 # Grant demo secret creator role
 (
   . ~/devstack/openrc admin demo
