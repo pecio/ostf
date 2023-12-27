d952a25bdffcc0c457ad5b6aebec0fc0ca17ee4b
Raúl Pedroche <pedroche@me.com>
2023-12-27 19:58:14 +0100

    Make use of k8snode image

--- k8s/nodes.tf
+++ k8s/nodes.tf
@@ -2,7 +2,7 @@ resource "openstack_compute_instance_v2" "node" {
   count = var.nodes
 
   name        = "node${count.index}-${random_pet.suffix.id}"
-  image_name  = "jammy-server-cloudimg-amd64-disk-kvm"
+  image_name  = "k8snode"
   flavor_name = "m1.medium"
   key_pair    = openstack_compute_keypair_v2.keypair.name
   security_groups = [
