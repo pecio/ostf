23728ebd130a6eb8ff789396acea614823e33f98
Raúl Pedroche <pedroche@me.com>
2023-12-30 12:55:17 +0100

    Add harbor_ip and suffix variables to inventory

--- k8s/ansible.tf
+++ k8s/ansible.tf
@@ -12,6 +12,10 @@ resource "ansible_host" "node" {
 
 resource "ansible_group" "k8s" {
   name = "k8s"
+
+  variables = {
+    harbor_ip = openstack_compute_instance_v2.harbor.access_ip_v4
+  }
 }
 
 resource "ansible_group" "all" {
@@ -20,6 +24,8 @@ resource "ansible_group" "all" {
   variables = {
     ansible_ssh_private_key_file = local_sensitive_file.private_key.filename
     ansible_user                 = "ubuntu"
+
+    suffix = random_pet.suffix.id
   }
 }
 
